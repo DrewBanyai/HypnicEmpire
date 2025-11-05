c1ce73ffa514f6652bf932e8a6ae42f01fd5e785
Drew Banyai <DrewBanyai@gmail.com>
2025-11-05 00:15:27 -0500

    - Added new updates to the Localization class

--- Assets/Scripts/Localization.cs
+++ Assets/Scripts/Localization.cs
@@ -9,6 +9,22 @@ namespace HypnicEmpire
         public static string DisplayText_ResourceChangeDisplayAmount(int amount) => $"{DisplayText_PositiveNumberPlus(amount)}{amount}";
         public static string DisplayText_ResourceCountDivide(int amount, int max) => $"{amount} / {max}";
         public static string DisplayText_CurrentLevelAndMax(int currentLevel, int maxLevel) => $"{currentLevel + 1} / {maxLevel + 1}";
+        public static string DisplayText_LevelExplorationPercent(int percent) => $"Level Exploration: {percent}%";
+
+        public static string DisplayText_ActionName(PlayerActionType actionType)
+        {
+            switch (actionType)
+            {
+                case PlayerActionType.Delve: return "Delve";
+                case PlayerActionType.Forage: return "Forage";
+                case PlayerActionType.Hunting: return "Hunting";
+                case PlayerActionType.Sell_Food: return "Sell Food";
+                case PlayerActionType.Trade_Herbs: return "Trade Herbs";
+                case PlayerActionType.Market: return "Market";
+                case PlayerActionType.Chop_Wood: return "Chop Wood";
+            }
+            return "Unknown Action";
+        }
 
         public static string JournalEntry_GameSaved() => "*GAME SAVED*";
     }
