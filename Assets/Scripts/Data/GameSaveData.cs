385bf8acd24c2970c7e7534e56fd57422fd6e35b
Drew Banyai <DrewBanyai@gmail.com>
2026-02-08 06:59:08 -0500

    Added a GameSaveData class to hold both game state and game unlock list

---
+++ Assets/Scripts/Data/GameSaveData.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HypnicEmpire
+{
+    [Serializable]
+    public class GameSaveData
+    {
+        public GameState GameState;
+        public SerializableDictionary<string, bool> GameUnlockList;
+    }
+}
